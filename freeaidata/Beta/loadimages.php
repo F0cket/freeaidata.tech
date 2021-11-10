d4f5f872d7e811f4a8418396c4be7992c3dff81e
F0cket <78611790+F0cket@users.noreply.github.com>
2021-11-10 09:24:48 -0600

    Delete freeaidata/Beta directory

--- freeaidata/Beta/loadimages.php
+++
@@ -1,9 +0,0 @@
-<?php
-$files = scandir('uploads/');
-foreach($files as $file) {
-    if($file !== "." && $file !== "..") {
-        //echo ("<img src='uploads/$file' />");
-        echo("<a href='uploads/$file'>$file</a>");
-    }
-}
-?>
